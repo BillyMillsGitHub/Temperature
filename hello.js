eb6d5cc2678b88e4c35c906216a1e477ad650d22
Billy Mills <bmills94@outlook.com>
2022-11-22 16:33:15 +0000

    Updated hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("hello");
+//setting the variable of kelvin to a const as this variable needs to remain constant
+const kelvin = 0;
+//setting celsius to constant
+const celsius = kelvin - 273;
+//calculating value of fahrenheit through celsius
+const fahrenheit = celsius * (9/5) + 32;
+console.log(Math.floor(fahrenheit));//rounding to close whole number
+//Interpolation of string and variable value
+console.log(`The temperature in ${fahrenheit} degrees fahrenheit`);
